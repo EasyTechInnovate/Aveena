9788d9842c5c8c014774bdf329ea469b8bd29758
Harsh <harshgupta20220@gmail.com>
2025-12-17 00:09:19 +0530

    changes

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const baseURL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+  import.meta.env.VITE_API_URL 
 
 if (!baseURL) {
   throw new Error("VITE_API_URL environment variable is not defined.");
